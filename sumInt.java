d450f44bddc21d421854e9a469f83eda95008432
parveenkhan <parveen.khan@thoughtworks.com>
2022-01-15 19:03:50 +0000

    Sum of integers

--- sumInt.java
+++ sumInt.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class sumInt {
+public class sumInt {
+    public static void main(String[] args) {
+
+        int[] arry = new int[]{1,2,3,4,10,11};
+        int sum = 0;
+        //System.out.println(arry[2]);
+        //System.out.println(arry.length);
+        for(int i=0;i<arry.length;i++) {
+            //Prints the array
+            //System.out.println(arry[i]);
+            sum+=arry[i];
+        }
+        System.out.println("The sum of array is: "+sum);
+
+    }
 }
