648dee2a71fb7cdd12a3da916b7f838b55bde027
parveenkhan <parveen.khan@thoughtworks.com>
2022-01-18 12:28:05 +0000

    committing remaining changes

--- palindrome.java
+++ palindrome.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class palindrome {
+public class palindrome {
+
+    public static void main(String[] args) {
+
+        String originalstr = "racecar";
+        String reversestr = "";
+
+        int strlength = originalstr.length();
+
+        //Reverse String
+        for(int i = strlength-1;i>=0;i--) {
+            // reversestr variable has the reversed string
+            reversestr = reversestr + originalstr.charAt(i);
+        }
+        //Comparing two strings using equals method
+        if(originalstr.equals(reversestr))
+            System.out.println("It's a palindrome");
+            else
+                System.out.println("It's not a palindrome");
+
+
+
+    }
 }
