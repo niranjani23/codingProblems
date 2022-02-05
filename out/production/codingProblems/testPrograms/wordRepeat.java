98d66e7399469c9f92d5baf9f83f8467b7e97fbb
parveenkhan <parveen.khan@thoughtworks.com>
2022-02-05 18:41:03 +0000

    Pushing the entire class

--- out/production/codingProblems/testPrograms/wordRepeat.java
+++ out/production/codingProblems/testPrograms/wordRepeat.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class wordRepeat {
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class wordRepeat {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = sc.nextLine();
+        System.out.println("You have entered:" + str);
+
+        String[] sp = str.split(" ");
+
+        Set<String> set = new HashSet<>();
+
+        // here we are iterating in Array
+        for (int i = 0; i < sp.length - 1; i++) {
+
+            for (int j = 1; j < sp.length; j++) {
+
+                if (sp[i].equals(sp[j]) && i != j) {
+                    set.add(sp[i]);
+                }
+            }
+        }
+
+        // printing the set
+        System.out.println("Words that have been repeated are:"+set);
+    }
 }
