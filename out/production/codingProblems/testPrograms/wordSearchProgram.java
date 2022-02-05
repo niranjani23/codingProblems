922ed131ecf569c4fad8a56f50cb9a85e5e697a6
Niranjani Manoharan <nmanoharan@lyft.com>
2022-02-05 10:45:35 -0800

    Revert "Fixing problems Again"

--- out/production/codingProblems/testPrograms/wordSearchProgram.java
+++ out/production/codingProblems/testPrograms/wordSearchProgram.java
@@ -1,17 +1,2 @@
-import java.util.Scanner;
-
-public class wordSearchProgram {
-
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter a String: ");
-        String str= sc.nextLine();
-        System.out.println("You have entered:"+str);
-
-        String[] sp=str.split(" ");
-        //System.out.println("second word is :" + sp[1]);
-        String sl=sp[sp.length - 2];
-        System.out.println("Second last word is :" + sl);
-
-    }
+package PACKAGE_NAME;public class wordSearchProgram {
 }
